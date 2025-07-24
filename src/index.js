9edf2a672573881b2191be4449fd5a4a4cee7e86
Sheila Kagawa <145503159+shlqjn@users.noreply.github.com>
2025-07-24 13:15:47 -0700

    added basic form handling and added Typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+function generateQuote(event) {
+  event.preventDefault();
 
+  new Typewriter("#quotes", {
+    strings: "Nothing is impossible, the word itself says 'I'm possible'",
+    autoStart: true,
+    delay: 1,
+  });
+}
+
+let quoteFormElement = document.querySelector("#quote-generator-form");
+quoteFormElement.addEventListener("submit", generateQuote);
